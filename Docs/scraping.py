7c432a73f95369800bc6370efeddb8b0aef28080
anass dermaj <anassdermaj163@gmail.com>
2023-10-30 17:36:11 +0100

    30-10-2023-18:36

--- Docs/scraping.py
+++ Docs/scraping.py
@@ -40,17 +40,21 @@ url=links_list[0][0]
 print(url)
 driver=webdriver.Chrome()
 driver.get(str(url))
-title=driver.find_element(By.CLASS_NAME,'title--wrap--Ms9Zv4A')
-title=title.find_element(By.TAG_NAME,'h1').text
+#title
+# title=driver.find_element(By.CLASS_NAME,'title--wrap--Ms9Zv4A')
+# title=title.find_element(By.TAG_NAME,'h1').text
+# #price
+# price=driver.find_element(By.CLASS_NAME , 'es--wrap--erdmPRe')
+# sp=price.find_elements(By.TAG_NAME,"span")
+# price=''
+# for i in range(2,len(sp)):
+#     price+=sp[i].text
+# price=float(price)
+content=driver.find_element(By.CSS_SELECTOR,'#product-description')
+# content=driver.page_source
+# print(content.text)
 
-price=driver.find_element(By.CLASS_NAME , 'es--wrap--erdmPRe')
-price=driver.find_elements(By.TAG_NAME,'span')
-price_total=''
-for i in range(2,len(price)):
-    price_total+=price[i].text
-content=driver.find_element(By.CLASS_NAME,'extend--content--DlnAngg')
-
-print(price_total)
-print(title)
-print(content.get_attribute('innerHTML'))
+# with open('Docs/readme.txt', 'w',encoding='utf8') as f:
+#     f.write(content)
+print(content)
 
