658ef1b79df1455b9dc5df5fd61903396fd27775
anass dermaj <anassdermaj163@gmail.com>
2023-11-14 11:53:33 +0100

    14-11-2023-12:53

--- Dashboard/update.php
+++ Dashboard/update.php
@@ -105,11 +105,13 @@ if (isset($_POST['operation'])) {
                 }
             }
             echo "blog is updated successfully";
-        } else {
+        } 
+        else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
         $conn->close();
-    } else if ($operation == 'product') {
+    }
+    else if ($operation == 'product') {
         $id = $_POST["id"];
         $query = "select * from product where PRODUCT_ID=$id";
         $result = $conn->query($query);
@@ -203,11 +205,13 @@ if (isset($_POST['operation'])) {
             // }
 
             echo "Product is updated successfully";
-        } else {
+        }
+        else {
             echo "Error: " . $query . "<br>" . $conn->error;
         }
         $conn->close();
-    } else if ($operation == 'sponsor') {
+    } 
+    else if ($operation == 'sponsor') {
         $id = $_POST["id"];
         $query = "select * from sponsor where SPONSOR_ID=$id";
         $result = $conn->query($query);
@@ -240,67 +244,69 @@ if (isset($_POST['operation'])) {
         if ($conn->query($query) === TRUE) {
 
             echo "Sponsor is updated successfully";
-        } else {
+        }
+        else {
             echo "Error: " . $query . "<br>" . $conn->error;
         }
         $conn->close();
     }
-    
-  }
-  else if ($operation == 'user') {
-    $id = $_POST["id"];
-    $query = "select * from user where USER_ID=$id";
-    $result = $conn->query($query);
-    $row = mysqli_fetch_assoc($result);
-    // $first_name = $row["FIRST_NAME"];
-    // $last_name = $row["LAST_NAME"];
-    // $email = $row["EMAIL"];
-    // $password = $row["PASSWORD"];
-    // $phone_number = $row["PHONE_NUMBER"];
-    $first_name =!empty($_POST['first_name']) ? $_POST['first_name'] :$row["FIRST_NAME"];
-    $last_name = !empty($_POST['last_name']) ? $_POST['last_name'] :$row["LAST_NAME"];
-    $email = !empty($_POST['email']) ? $_POST['email'] :$row["EMAIL"];
-    $password = !empty($_POST['password']) ? $_POST['password'] :$row["PASSWORD"];
-    $phone_number = !empty($_POST['phone_number']) ? $_POST['phone_number'] :$row["PHONE_NUMBER"];
-     
-    // if (isset($_POST['first_name'])) {
-    //     $first_name = $_POST['first_name'];
-    // }
-    // if (isset($_POST['last_name'])) {
-    //     $last_name = $_POST['last_name'];
-    // }
-    // if (isset($_POST['email'])) {
-    //     $email = $_POST['email'];
-    // }
-    // if (isset($_POST['password'])) {
-    //     $password = $_POST['password'];
-    // }
-    // if (isset($_POST['phone_number'])) {
-    //     $phone_number = $_POST['phone_number'];
-    // }
-    
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    else if ($operation == 'user') {
+        $id = $_POST["id"];
+        $query = "select * from user where USER_ID=$id";
+        $result = $conn->query($query);
+        $row = mysqli_fetch_assoc($result);
+        // $first_name = $row["FIRST_NAME"];
+        // $last_name = $row["LAST_NAME"];
+        // $email = $row["EMAIL"];
+        // $password = $row["PASSWORD"];
+        // $phone_number = $row["PHONE_NUMBER"];
+        $first_name =!empty($_POST['first_name']) ? $_POST['first_name'] :$row["FIRST_NAME"];
+        $last_name = !empty($_POST['last_name']) ? $_POST['last_name'] :$row["LAST_NAME"];
+        $email = !empty($_POST['email']) ? $_POST['email'] :$row["EMAIL"];
+        $password = !empty($_POST['password']) ? $_POST['password'] :$row["PASSWORD"];
+        $phone_number = !empty($_POST['phone_number']) ? $_POST['phone_number'] :$row["PHONE_NUMBER"];
+        
+        // if (isset($_POST['first_name'])) {
+        //     $first_name = $_POST['first_name'];
+        // }
+        // if (isset($_POST['last_name'])) {
+        //     $last_name = $_POST['last_name'];
+        // }
+        // if (isset($_POST['email'])) {
+        //     $email = $_POST['email'];
+        // }
+        // if (isset($_POST['password'])) {
+        //     $password = $_POST['password'];
+        // }
+        // if (isset($_POST['phone_number'])) {
+        //     $phone_number = $_POST['phone_number'];
+        // }
+        
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+
+        $first_name = mysqli_real_escape_string($conn, $first_name);
+        $last_name = mysqli_real_escape_string($conn, $first_name);
+        $email = mysqli_real_escape_string($conn, $first_name);
+        $password = mysqli_real_escape_string($conn, $first_name);
+        $phone_number = mysqli_real_escape_string($conn, $first_name);
+        $date = date('Y-m-d-h:i:sa');
+        
+        $query = "UPDATE user SET FIRST_NAME='$first_name',LAST_NAME='$last_name',EMAIL='$email',PASSWORD='$password',PHONE_NUMBER='$phone_number',UPDATE_DATE='$date'   where USER_ID=$id";
+
+        // $conn->query($query);
+        if ($conn->query($query) === TRUE) {
+
+            echo "User is updated successfully";
+        }
+        else {
+            echo "Error: " . $query . "<br>" . $conn->error;
+        }
+        $conn->close();
     }
-
-    $first_name = mysqli_real_escape_string($conn, $first_name);
-    $last_name = mysqli_real_escape_string($conn, $first_name);
-    $email = mysqli_real_escape_string($conn, $first_name);
-    $password = mysqli_real_escape_string($conn, $first_name);
-    $phone_number = mysqli_real_escape_string($conn, $first_name);
-    $date = date('Y-m-d-h:i:sa');
-    
-    $query = "UPDATE user SET FIRST_NAME='$first_name',LAST_NAME='$last_name',EMAIL='$email',PASSWORD='$password',PHONE_NUMBER='$phone_number',UPDATE_DATE='$date'   where USER_ID=$id";
-
-    // $conn->query($query);
-    if ($conn->query($query) === TRUE) {
-
-        echo "User is updated successfully";
-    } else {
-        echo "Error: " . $query . "<br>" . $conn->error;
-    }
-    $conn->close();
 }
+  
 
 
  else {
--- Docs/amazon/test.py
+++ Docs/amazon/test.py
@@ -11,36 +11,52 @@ import requests
 import os
 
 
-url='https://www.amazon.com/Hempz-Body-Lotion-Moisturizing-Moisturizer/dp/B0BNP8TWQ9/ref=sr_1_139?keywords=Body+Skin+Care+Products&qid=1699869068&s=beauty&sr=1-139'
+url2='https://www.amazon.com/Hempz-Body-Lotion-Moisturizing-Moisturizer/dp/B0BNP8TWQ9/ref=sr_1_139?keywords=Body+Skin+Care+Products&qid=1699869068&s=beauty&sr=1-139'
+# driver=webdriver.Chrome()
+# driver.set_window_position(-10000,0)
+# driver.get(str(url))
+
+# title=driver.find_element(By.ID,'productTitle').text
+# price_section=driver.find_element(By.CLASS_NAME,'reinventPricePriceToPayMargin')
+# price=price_section.find_element(By.CLASS_NAME,'a-price-whole').text
+# price_dot=price_section.find_element(By.CLASS_NAME,'a-price-fraction').text
+# # price=price.replace('$','')
+# price=str(price)+'.'+str(price_dot)
+# price=float(price)
+# desc1=driver.find_element(By.ID,'productOverview_feature_div')
+# desc1=desc1.find_element(By.CLASS_NAME,'a-section')
+# desc2=driver.find_element(By.ID,'featurebullets_feature_div')
+
+# print(title)
+# print(price)
+# img_grid=driver.find_element(By.CLASS_NAME,'regularAltImageViewLayout')
+# imgs=img_grid.find_elements(By.TAG_NAME,'img')
+# list_img=[]
+# for img in imgs:
+#     new_img=img.get_attribute('src')
+#     if 'play-icon-overlay' not in new_img and 'icon' not in new_img:
+#         new_img=new_img.split('._')
+#         new_img[1]=new_img[1].split('.')
+#         img_result=new_img[0]+'.'+new_img[1][1]
+#         list_img.append(img_result) 
+
+
+
+url='https://rankactive.com/resources/generate-keywords'
 driver=webdriver.Chrome()
 driver.set_window_position(-10000,0)
 driver.get(str(url))
 
-title=driver.find_element(By.ID,'productTitle').text
-price_section=driver.find_element(By.CLASS_NAME,'reinventPricePriceToPayMargin')
-price=price_section.find_element(By.CLASS_NAME,'a-price-whole').text
-price_dot=price_section.find_element(By.CLASS_NAME,'a-price-fraction').text
-# price=price.replace('$','')
-price=str(price)+'.'+str(price_dot)
-price=float(price)
-desc1=driver.find_element(By.ID,'productOverview_feature_div')
-desc2=driver.find_element(By.ID,'featurebullets_feature_div')
-full_desc=driver.find_element(By.ID,'aplus_feature_div')
-print(desc1.get_attribute('innerHTML'))
-print(desc2.get_attribute('innerHTML'))
-print(full_desc.get_attribute('innerHTML'))
-print(title)
-print(price)
-img_grid=driver.find_element(By.CLASS_NAME,'regularAltImageViewLayout')
-imgs=img_grid.find_elements(By.TAG_NAME,'img')
-list_img=[]
-for img in imgs:
-    new_img=img.get_attribute('src')
-    if 'play-icon-overlay' not in new_img and 'icon' not in new_img:
-        new_img=new_img.split('._')
-        new_img[1]=new_img[1].split('.')
-        img_result=new_img[0]+'.'+new_img[1][1]
-        list_img.append(img_result) 
-    # print(new_img)
-for x in list_img:
-    print(x)
+input_txt=driver.find_element(By.ID,'searchQuery')
+input_txt.send_keys(url2)
+btn=driver.find_element(By.ID,'formButton').click()
+time.sleep(200)
+result=driver.find_element(By.ID,'results')
+rows=driver.find_elements(By.CLASS_NAME,'res-keyword')
+keywords=''
+for x in rows:
+    keywords+=x.text+','
+    
+
+with open('Docs/amazon/keywords.txt','w') as file_key:
+    file_key.write(keywords)
